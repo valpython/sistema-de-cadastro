7016dccbefee604a2483f6d9481553e911d12efb
valpython <114414268+valpython@users.noreply.github.com>
2023-02-10 17:11:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+console.log("git commit app.js -m voltando para o estado do paulo")
